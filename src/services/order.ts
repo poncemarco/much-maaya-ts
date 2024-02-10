276e114cd5e9fc0bf25767bb81a26fb44210b0f2
Marco Ponce <marco_cap@192.168.1.4>
2024-02-09 22:04:34 -0600

    brava neta

--- src/services/order.ts
+++ src/services/order.ts
@@ -1,8 +1,8 @@
-import { type Order, type NewOrder } from '../types/api';
+import { type NewOrder } from '../types/api';
 import { SITE_URL } from '../consts';
 
 export const sendOrder = async (data : NewOrder) => {
-    const url = `${SITE_URL}/api/v1/orders/`;
+    const url = 'https://rama-ws.com/api/v1/orders/';
     console.log(url);
     const res = await fetch(url, {
         method: 'POST',
