51e0deeb060e304cc3fbfce221d885f2fd08de74
Marco Ponce <marco_cap@192.168.1.4>
2024-02-12 20:33:34 -0600

    new deployment with units

---
+++ .vercel/output/functions/_render.func/.vercel/output/_functions/chunks/_.._nwiKAFhR.mjs
@@ -0,0 +1,5 @@
+export { renderers } from '../renderers.mjs';
+
+const page = () => import('./prerender_t8lDb5H1.mjs').then(n => n.b);
+
+export { page };
--- .vercel/output/functions/_render.func/.vercel/output/_functions/chunks/ticket_8UCt9u1n.mjs
+++
@@ -1,5 +0,0 @@
-export { renderers } from '../renderers.mjs';
-
-const page = () => import('./prerender_QO0Thgjy.mjs').then(n => n.t);
-
-export { page };
