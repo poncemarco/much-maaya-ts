8e8f456e77414f1384cbb93be49cd84004abd799
Marco Ponce <marco_cap@192.168.1.3>
2024-03-24 19:55:02 -0600

    version 1.2

---
+++ .vercel/output/functions/_render.func/.vercel/output/_functions/chunks/404_tsz5sI4a.mjs
@@ -0,0 +1,5 @@
+export { renderers } from '../renderers.mjs';
+
+const page = () => import('./prerender_mQ1PpG5i.mjs').then(n => n._);
+
+export { page };
--- .vercel/output/functions/_render.func/.vercel/output/_functions/entry.mjs
+++ .vercel/output/functions/_render.func/.vercel/output/_functions/entry.mjs
@@ -1,5 +1,5 @@
 import { renderers } from './renderers.mjs';
-import { l as levels, g as getEventPrefix, L as Logger, A as AstroIntegrationLogger, manifest } from './manifest_eUPxwnbf.mjs';
+import { l as levels, g as getEventPrefix, L as Logger, A as AstroIntegrationLogger, manifest } from './manifest_yR0m4hlf.mjs';
 import 'node:fs';
 import { e as appendForwardSlash, j as joinPaths, t as trimSlashes, s as slash, p as prependForwardSlash, r as removeTrailingForwardSlash, f as collapseDuplicateSlashes } from './chunks/astro/assets-service_KwutD9rH.mjs';
 import { R as ROUTE_DATA_SYMBOL, A as AstroError, n as ResponseSentError, o as MiddlewareNoDataOrNextCalled, p as MiddlewareNotAResponse, G as GetStaticPathsRequired, q as InvalidGetStaticPathsReturn, t as InvalidGetStaticPathsEntry, u as GetStaticPathsExpectedParams, v as GetStaticPathsInvalidRouteParam, P as PageNumberParamNotFound, N as NoMatchingStaticPathFound, w as PrerenderDynamicEndpointPathCollide, x as LocalsNotAnObject, y as ASTRO_VERSION, C as ClientAddressNotAvailable, S as StaticClientAddressNotAvailable, z as renderEndpoint, B as ReservedSlotName, D as renderSlotToString, H as renderJSX, J as chunkToString, K as CantRenderPage, O as renderPage$1, Q as REROUTE_DIRECTIVE_HEADER } from './chunks/astro_HIuUkYty.mjs';
@@ -2194,19 +2194,19 @@ const createExports = (manifest, { middlewareSecret }) => {
 };
 
 const _page0 = () => import('./chunks/generic_X-Ug6Cbt.mjs');
-const _page1 = () => import('./chunks/404_TXwkdu7T.mjs');
-const _page2 = () => import('./chunks/carrito_Dz_ZGtCd.mjs');
-const _page3 = () => import('./chunks/_slug__bLnyRtEp.mjs');
-const _page4 = () => import('./chunks/_.._5woyIaWj.mjs');
-const _page5 = () => import('./chunks/_page__zwvQlkz7.mjs');
-const _page6 = () => import('./chunks/_.._3tdAQ3MD.mjs');
-const _page7 = () => import('./chunks/index_Tw3NrVNJ.mjs');
-const _page8 = () => import('./chunks/contact_9nc9Le6a.mjs');
-const _page9 = () => import('./chunks/order_fNrJxOMj.mjs');
-const _page10 = () => import('./chunks/politicas_2CIt3RED.mjs');
-const _page11 = () => import('./chunks/robots_Mw9fRsVx.mjs');
-const _page12 = () => import('./chunks/search_WdWb4v3K.mjs');
-const _page13 = () => import('./chunks/tyc_4OoXcQrq.mjs');
+const _page1 = () => import('./chunks/404_tsz5sI4a.mjs');
+const _page2 = () => import('./chunks/carrito_ata40hTK.mjs');
+const _page3 = () => import('./chunks/_slug__l6oBwsmJ.mjs');
+const _page4 = () => import('./chunks/_.._PP9L1fVr.mjs');
+const _page5 = () => import('./chunks/_page__4gQGfPo9.mjs');
+const _page6 = () => import('./chunks/_.._4doBnJUw.mjs');
+const _page7 = () => import('./chunks/index_ZBpl4R9p.mjs');
+const _page8 = () => import('./chunks/contact_MIadwiWX.mjs');
+const _page9 = () => import('./chunks/order_2W0vauXK.mjs');
+const _page10 = () => import('./chunks/politicas_qcpQg-A-.mjs');
+const _page11 = () => import('./chunks/robots_iiBqDqo4.mjs');
+const _page12 = () => import('./chunks/search_OcEa_gmG.mjs');
+const _page13 = () => import('./chunks/tyc_C_fJ4t6-.mjs');
 const pageMap = new Map([
     ["node_modules/astro/dist/assets/endpoint/generic.js", _page0],
     ["src/pages/404.astro", _page1],
@@ -2230,7 +2230,7 @@ const _manifest = Object.assign(manifest, {
     middleware: onRequest
 });
 const _args = {
-    "middlewareSecret": "5aa45be0-a385-4c56-a43d-67c45eca1eb6"
+    "middlewareSecret": "00122a6a-bc34-49a3-af1a-6592c3997935"
 };
 const _exports = createExports(_manifest, _args);
 const __astrojsSsrVirtualEntry = _exports.default;
